92d447953af28d50cc961afe70562908049016e1
LEE-sh1673 <lsh901673@gmail.com>
2022-06-07 18:53:08 +0900

    Create `DocumentManagementSystem.java` for set up central class.

---
+++ src/main/java/org/lee_sh1673/DocumentManagementSystem.java
@@ -0,0 +1,61 @@
+package org.lee_sh1673;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.lee_sh1673.Attributes.WIDTH;
+
+public class DocumentManagementSystem {
+
+    private final List<Document> documents
+            = new ArrayList<>();
+    private final Map<String, Importer> extensionToImporter
+            = new HashMap<>();
+
+    public DocumentManagementSystem() {
+        //TODO: put more importer later (letter, report)
+        extensionToImporter.put("jpg", new ImageImporter());
+    }
+
+    public void importFile(final String path) throws IOException {
+        final File file = new File(path);
+
+        if (!file.exists()) {
+            throw new FileNotFoundException(path);
+        }
+
+        final int separatorIndex = path.lastIndexOf(".");
+
+        // If path is valid.
+        if (separatorIndex != -1) {
+
+            // If path has no extension file.
+            if (separatorIndex == path.length()) {
+                throw new UnknownFileTypeException("No extension found For file: " + path);
+            }
+            final String extension = path.substring(separatorIndex + 1);
+            final Importer importer = extensionToImporter.get(extension);
+
+            if (importer == null) {
+                throw new UnknownFileTypeException("For file: " + path);
+            }
+
+            final Document document = importer.importFile(file);
+            documents.add(document);
+        } else {
+            throw new UnknownFileTypeException("No extension found for file: " + path);
+        }
+    }
+
+    //TODO: This is temp method for test.
+    public void showDocumentWidthInfo() {
+        for (final Document document : documents) {
+            System.out.println(document.getAttribute(WIDTH));
+        }
+    }
+}
