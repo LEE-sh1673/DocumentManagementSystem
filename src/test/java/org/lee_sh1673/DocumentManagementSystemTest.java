1756d588f091efd338a28e9e20c00367ec437d67
LEE-sh1673 <lsh901673@gmail.com>
2022-06-09 11:28:35 +0900

    Create unit test of `DocumentManagementSystem` for testing `importFile` correctly.

--- src/test/java/org/lee_sh1673/DocumentManagementSystemTest.java
+++ src/test/java/org/lee_sh1673/DocumentManagementSystemTest.java
@@ -2,22 +2,33 @@ package org.lee_sh1673;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DocumentManagementSystemTest {
-    final static String RESOURCES = "src/main/resources/";
-    final static String[] FILE_NAMES = {"test.jpg", "test.letter", "test.report"};
+    final static String RESOURCES = "src" + File.separator
+            + "main" + File.separator
+            + "resources" + File.separator;
 
-    @Test void shouldImportFileCorrect() throws IOException {
-        DocumentManagementSystem documentManagementSystem
-                = new DocumentManagementSystem();
+    final static String LETTER = RESOURCES + "test.letter";
+    final static String REPORT = RESOURCES + "test.report";
+    final static String IMAGE = RESOURCES + "test.jpg";
+    
+    private final DocumentManagementSystem system = new DocumentManagementSystem();
 
-        for (final String fileName : FILE_NAMES) {
-            Path path = Paths.get(RESOURCES + fileName);
-            documentManagementSystem.importFile(path.toString());
-        }
-        documentManagementSystem.showDocumentsInfo();
+    @Test public void shouldImportFile() throws Exception {
+        system.importFile(LETTER);
+        final Document document = onlyDocument();
+        assertEquals(document.getAttribute(Attributes.PATH), LETTER);
+    }
+
+    private Document onlyDocument() {
+        final List<Document> documents = system.contents();
+        assertThat(documents, hasSize(1));
+        return documents.get(0);
     }
 }
